44b6519c929ddf9028e7a60c4920a90505cb2f26
Michael Mahoney <mikemahoney0492@gmail.com>
2017-11-23 17:05:03 +0000

    desk added

--- js/background.js
+++ js/background.js
@@ -4,11 +4,13 @@ var calculator = new Vue({
         price: '',
         selected: '',
         currencySymbol: '€',
-        durationProjects: 'monthly',
-        durationDesk: 'monthly',
-        pricePlanProject: 'pro',
-        pricePlanDesk: 'pro',
+        durationProjects: '',
+        durationDesk: '',
+        pricePlanProject: '',
+        pricePlanDesk: '',
         users: '5',
+        agents: '0',
+        helpdocSites: '0',
         projectsSelected: false,
         deskSelected: false,
         eduDiscount: false,
@@ -17,21 +19,38 @@ var calculator = new Vue({
     computed: {
         calcPayment: function(e){
             var payment = 0;
+
+            //Calculate Projects totals
             if(this.projectsSelected){
-                payment = parseInt(this.duration, 10) * parseInt(this.users, 10);
-                if(this.eduDiscount == true){
-                    payment = payment - (payment * 0.15);
-                } else {
-                    payment = payment - (payment * (this.customDiscount/100));
-                }
+                payment += parseInt(this.durationProjects, 10) * parseInt(this.users, 10);
             }
+
+            //Calculate Desk totals
             if(this.deskSelected){
-                if(this.eduDiscount == true){
-                    payment = payment - (payment * 0.15);
-                } else {
-                    payment = payment - (payment * (this.customDiscount/100));
+                var planPrice = 0;
+                if(this.durationDesk == 'monthly' && this.pricePlan == 'basic'){
+                    planPrice = 7;
+                } else if (this.durationDesk == 'monthly' && this.pricePlanDesk == 'pro'){
+                    planPrice = 5;
+                } else if (this.durationDesk == 'annual' && this.pricePlanDesk == 'basic'){
+                    planPrice = 30;
+                } else if (this.durationDesk == 'annual' && this.pricePlanDesk == 'pro'){
+                    planPrice = 20;
                 }
+                var helpdocSitesTotal = 0;
+                if(parseInt(this.helpdocSites, 10) > 1){
+                    helpdocSitesTotal = parseInt(this.helpdocSites, 10) * 20;
+                }
+                payment += (parseInt(planPrice, 10) * parseInt(this.agents, 10)) + helpdocSitesTotal;
             }
+
+            //Subtract discounts
+            if(this.eduDiscount == true){
+                payment = payment - (payment * 0.15);
+            } else {
+                payment = payment - (payment * (this.customDiscount/100));
+            }
+
             return this.currencySymbol + payment;
         }
     }
