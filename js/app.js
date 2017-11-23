e824c09c77f8e2a24832f0c91efdb8f522f8acd2
Michael Mahoney <mikemahoney0492@gmail.com>
2017-11-23 14:37:41 +0000

    projects working

--- js/app.js
+++ js/app.js
@@ -2,29 +2,26 @@ var calculator = new Vue({
     el: '#calculator',
     data: {
         price: '',
-        downPayment: '',
-        tradeIn: '',
-        length: '60',
-        rate: '',
         selected: '',
-        currencySymbol: '€'
+        currencySymbol: '€',
+        duration: 'monthly',
+        pricePlan: 'pro',
+        users: '5',
+        projectsSelected: false,
+        deskSelected: false,
+        eduDiscount: false,
+        customDiscount: ''
     },
     computed: {
         calcPayment: function(e){
-            var p = this.price - this.downPayment - this.tradeIn;
-            var r = this.rate / 1200;
-            var n = this.length;
-            var i = Math.pow((1+r),n);
-            var payment = ( p * r * i) / (i - 1) || 0;
-            return currencyFormat(payment, this);
-        },
-        numFormat: function(e){
-            e.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, this.currencySymbol + "1,");
+            var payment = parseInt(this.duration, 10) * parseInt(this.users, 10);
+            if(this.eduDiscount == true){
+                payment = payment - (payment * 0.15);
+            } else {
+                payment = payment - (this.customDiscount/100);
+            }
+            return this.currencySymbol + payment;
         }
     }
 
 });
-
-function currencyFormat (num, calculator) {
-    return calculator.currencySymbol + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, calculator.currencySymbol + "1,")
-}
