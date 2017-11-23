347cd90f6ab22e95645008cf47603359fbc34426
jacknev13 <jack.nevalainen@teamwork.com>
2017-11-23 15:58:12 +0000

    s

--- js/background.js
+++ js/background.js
@@ -4,8 +4,10 @@ var calculator = new Vue({
         price: '',
         selected: '',
         currencySymbol: '€',
-        duration: 'monthly',
-        pricePlan: 'pro',
+        durationProjects: 'monthly',
+        durationDesk: 'monthly',
+        pricePlanProject: 'pro',
+        pricePlanDesk: 'pro',
         users: '5',
         projectsSelected: false,
         deskSelected: false,
@@ -14,11 +16,21 @@ var calculator = new Vue({
     },
     computed: {
         calcPayment: function(e){
-            var payment = parseInt(this.duration, 10) * parseInt(this.users, 10);
-            if(this.eduDiscount == true){
-                payment = payment - (payment * 0.15);
-            } else {
-                payment = payment - (payment * (this.customDiscount/100));
+            var payment = 0;
+            if(this.projectsSelected){
+                payment = parseInt(this.duration, 10) * parseInt(this.users, 10);
+                if(this.eduDiscount == true){
+                    payment = payment - (payment * 0.15);
+                } else {
+                    payment = payment - (payment * (this.customDiscount/100));
+                }
+            }
+            if(this.deskSelected){
+                if(this.eduDiscount == true){
+                    payment = payment - (payment * 0.15);
+                } else {
+                    payment = payment - (payment * (this.customDiscount/100));
+                }
             }
             return this.currencySymbol + payment;
         }
